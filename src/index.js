a5abe7e75f067069457bae37a00ff73edec8d291
Rajat-Jaisawal-8009 <158314751+Rajat-Jaisawal-8009@users.noreply.github.com>
2024-06-26 00:14:29 +0530

    prob basename

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Route, RouterProvider,  createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import WeatherApp from './components/WeatherApp';
 import Main from './Main';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route basename='/rajat-weather-app' path='/' element={<Main />}>
+    <Route basename='/rajat-weather-app' path='/rajat-weather-app' element={<Main />}>
 
-    <Route path='/' element={<WeatherApp />}/>
+    <Route path='/rajat-weather-app' element={<WeatherApp />}/>
 
     </Route>
   )
@@ -21,7 +21,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<RouterProvider basename='/rajat-weather-app' router={router} />
+<RouterProvider  router={router} />
   </React.StrictMode>
 );
 
